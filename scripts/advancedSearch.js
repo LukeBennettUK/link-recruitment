daf5c6331875807c75d8984afc9f4222fbd2230b
LukeBennettUK <luke5495@gmail.com>
2018-05-09 10:46:11 +0100

    File Upload
    File Upload

---
+++ scripts/advancedSearch.js
@@ -0,0 +1,8 @@
+function openForm() {
+    var x = document.getElementById("advancedList");
+    if (x.style.display = "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
